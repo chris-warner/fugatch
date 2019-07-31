9ae3fdd8e33e8271581787a15a5e818fc9970d34
Christopher Warner <christopher.warner92@gmail.com>
2019-07-31 08:05:46 -0400

    auth bypassed as requested.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -31,7 +31,7 @@ module.exports = function(app) {
   });
 
 
-
+  //temp. disabled as requested.
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", function(req, res) {
